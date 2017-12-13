4de4c9cf9b4998b558cf6f2dd2cff31fac3e4507
Muzi (zuluCoda) <muzikayise.buthelezi@liberty.co.za>
2017-12-13 09:40:12 +0200

    [muzi] initial commit :fire:

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class App extends Component<{}> {
+    render() {
+        return (
+            <View style={ styles.container }>
+              <View style={ styles.box } />
+              <View style={ styles.box } />
+              <View style={ styles.box } />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'stretch'
+    },
+    box: {
+        width: 50,
+        backgroundColor: '#e76e63',
+        margin: 10
+    }
+});
+
